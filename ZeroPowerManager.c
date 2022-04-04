a9e186f80cbedf373b5934a2877aa3cd60e825e0
Hayward <hayward@smu.edu>
2022-04-03 21:08:37 -0500

    Updated w/ power to PROM

--- ZeroPowerManager.c
+++ ZeroPowerManager.c
@@ -293,7 +293,7 @@ void zpmRTCInit(void) {
 ******************************************************************************/
 void zpmSleep(void) {
     SCB->SCR |=  SCB_SCR_SLEEPDEEP_Msk;
-	//NVMCTRL->CTRLB.bit.SLEEPPRM = NVMCTRL_CTRLB_SLEEPPRM_DISABLED_Val; //cth - make sure FLASH does not pwoer all the way down in sleep (no effect)
+	NVMCTRL->CTRLB.bit.SLEEPPRM = NVMCTRL_CTRLB_SLEEPPRM_DISABLED_Val; //cth - make sure FLASH does not pwoer all the way down in sleep (no effect)
     SysTick->CTRL &= ~SysTick_CTRL_TICKINT_Msk; //cth to fix hangs
 	//PM->SLEEP.reg = 2; //idle mode (no effect)
     __DSB();
